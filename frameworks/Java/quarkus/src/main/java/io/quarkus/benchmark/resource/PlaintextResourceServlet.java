1e5c43c03bb32a90a7eb71eaa2e7f2ae163027a2
John OHara <johara@redhat.com>
2019-07-01 22:33:52 +0100

    Revert "Send Response in filter"
    This reverts commit 997f4e358d304abfaed0459a1fcbf5be79f5291d.

--- frameworks/Java/quarkus/src/main/java/io/quarkus/benchmark/resource/PlaintextResourceServlet.java
+++ frameworks/Java/quarkus/src/main/java/io/quarkus/benchmark/resource/PlaintextResourceServlet.java
@@ -10,11 +10,12 @@ import java.io.IOException;
 
 @WebServlet("/plaintext")
 public class PlaintextResourceServlet extends HttpServlet {
+    private static final String HELLO = "Hello, World!";
 
     @Override
     protected void doGet(HttpServletRequest reqest, HttpServletResponse response)
             throws ServletException, IOException {
-        //do nothing
+        response.getWriter().println(HELLO);
     }
 
 }
