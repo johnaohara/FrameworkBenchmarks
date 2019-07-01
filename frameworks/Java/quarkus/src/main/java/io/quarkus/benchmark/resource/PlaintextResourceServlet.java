29f6d96db1130ebdd66956c70bcc7e1a3a94967b
John OHara <johara@redhat.com>
2019-07-01 22:51:23 +0100

    revert changes to plaintext

--- frameworks/Java/quarkus/src/main/java/io/quarkus/benchmark/resource/PlaintextResourceServlet.java
+++ frameworks/Java/quarkus/src/main/java/io/quarkus/benchmark/resource/PlaintextResourceServlet.java
@@ -10,11 +10,14 @@ import java.io.IOException;
 
 @WebServlet("/plaintext")
 public class PlaintextResourceServlet extends HttpServlet {
-    private static final String HELLO = "Hello, World!";
+    private static final String HELLO = "hello, world!";
+    private static final String CONTENT_TYPE_HEADER = io.undertow.util.Headers.CONTENT_TYPE.toString();
+    private static final String CONTENT_TYPE = "text/plain; charset=utf-8";
 
     @Override
     protected void doGet(HttpServletRequest reqest, HttpServletResponse response)
             throws ServletException, IOException {
+        response.setHeader(CONTENT_TYPE_HEADER, CONTENT_TYPE);
         response.getWriter().println(HELLO);
     }
 
