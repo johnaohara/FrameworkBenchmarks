f2e573d2b7d3eaeb01bc82f6744bfb3588f4b97f
John OHara <johara@redhat.com>
2019-07-01 22:04:10 +0100

    Remove CompletableFuture from json

--- frameworks/Java/quarkus/src/main/java/io/quarkus/benchmark/resource/JsonResource.java
+++ frameworks/Java/quarkus/src/main/java/io/quarkus/benchmark/resource/JsonResource.java
@@ -5,17 +5,20 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
+import java.util.TreeMap;
 
 @Path("/json")
 public class JsonResource {
-    private static final String MESSAGE = "message";
-    private static final String HELLO = "Hello, World!";
+    private static final Map<String, String> message;
+    static {
+        message = new TreeMap<>();
+        message.put("message", "Hello, World!");
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public CompletionStage<Map<String, String>> json() {
-        return CompletableFuture.supplyAsync(() -> Map.of(MESSAGE, HELLO));
+    public Map json() {
+        return message;
     }
+
 }
