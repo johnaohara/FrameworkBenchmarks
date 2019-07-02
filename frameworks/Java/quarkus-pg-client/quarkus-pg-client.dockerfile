5b874c9170b1164d0fb14b3eb0d705d43957cdbf
Stephane Epardaud <stef@epardaud.fr>
2019-07-02 11:38:26 +0200

    Added quarkus-pg-client test

---
+++ frameworks/Java/quarkus-pg-client/quarkus-pg-client.dockerfile
@@ -0,0 +1,12 @@
+FROM maven:3.6.1-jdk-11-slim as maven
+WORKDIR /quarkus-pg-client
+COPY pom.xml pom.xml
+RUN mvn dependency:go-offline -q
+COPY src src
+RUN mvn package -q
+
+FROM openjdk:11.0.3-jdk-slim
+WORKDIR /quarkus-pg-client
+COPY --from=maven /quarkus-pg-client/target/lib lib
+COPY --from=maven /quarkus-pg-client/target/benchmark-pg-client-1.0-SNAPSHOT-runner.jar app.jar
+CMD ["java", "-server", "-XX:+UseNUMA", "-XX:+UseParallelGC", "-jar", "app.jar"]
---
+++ frameworks/Java/quarkus-pg-client/src/main/java/io/quarkus/benchmark/resource/DbResource.java
@@ -0,0 +1,85 @@
+package io.quarkus.benchmark.resource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import io.quarkus.benchmark.model.World;
+import io.quarkus.benchmark.repository.WorldRepository;
+
+@ApplicationScoped
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class DbResource {
+
+    @Inject
+    WorldRepository worldRepository;
+
+    @GET
+    @Path("/db")
+    public CompletionStage<World> db() {
+        return randomWorld();
+    }
+
+    @GET
+    @Path("/queries")
+    public CompletionStage<List<World>> queries(@QueryParam("queries") String queries) {
+        var worlds = new CompletableFuture[parseQueryCount(queries)];
+        var ret = new World[worlds.length];
+        Arrays.setAll(worlds, i -> {
+            return randomWorld().thenApply(w -> ret[i] = w);
+        });
+
+        return CompletableFuture.allOf(worlds).thenApply(v -> Arrays.asList(ret));
+    }
+
+    @GET
+    @Path("/updates")
+    public CompletionStage<List<World>> updates(@QueryParam("queries") String queries) {
+        var worlds = new CompletableFuture[parseQueryCount(queries)];
+        var ret = new World[worlds.length];
+        Arrays.setAll(worlds, i -> {
+            return randomWorld().thenCompose(w -> {
+                w.setRandomNumber(randomWorldNumber());
+                ret[i] = w;
+                return worldRepository.update(w);
+            });
+        });
+
+        return CompletableFuture.allOf(worlds).thenApply(v -> Arrays.asList(ret));
+    }
+
+    private CompletionStage<World> randomWorld() {
+        return worldRepository.find(randomWorldNumber());
+    }
+
+    private int randomWorldNumber() {
+        return 1 + ThreadLocalRandom.current().nextInt(10000);
+    }
+
+    private int parseQueryCount(String textValue) {
+        if (textValue == null) {
+            return 1;
+        }
+        int parsedValue;
+        try {
+            parsedValue = Integer.parseInt(textValue);
+        } catch (NumberFormatException e) {
+            return 1;
+        }
+        return Math.min(500, Math.max(1, parsedValue));
+    }
+}
