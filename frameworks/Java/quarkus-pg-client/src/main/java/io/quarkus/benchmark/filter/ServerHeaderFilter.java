5c372d79fdabeefa472cd19c7cd89978659f1875
Stephane Epardaud <stef@epardaud.fr>
2019-08-28 18:16:53 +0200

    Updated quarkus-pg-client to run on master

--- frameworks/Java/quarkus-pg-client/src/main/java/io/quarkus/benchmark/filter/ServerHeaderFilter.java
+++ frameworks/Java/quarkus-pg-client/src/main/java/io/quarkus/benchmark/filter/ServerHeaderFilter.java
@@ -1,15 +1,30 @@
 package io.quarkus.benchmark.filter;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
+import io.quarkus.scheduler.Scheduled;
+
+@ApplicationScoped
 @Provider
 public class ServerHeaderFilter implements ContainerResponseFilter {
+
+    private String date;
+
+    @Scheduled(every="1s")
+    void increment() {
+        date = DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now());
+    }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
         responseContext.getHeaders().add("Server", "Quarkus");
+        responseContext.getHeaders().add("Date", date);
     }
 }
