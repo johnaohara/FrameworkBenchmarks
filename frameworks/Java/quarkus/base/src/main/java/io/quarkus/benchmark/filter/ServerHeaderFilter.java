c348fb4eeff83a45a0a6b8c5ca07db4ae5bf58f4
Sanne Grinovero <sanne@hibernate.org>
2020-03-06 11:58:24 +0000

    Update the ServerHeaderFilter to use the platform scheduler

--- frameworks/Java/quarkus/base/src/main/java/io/quarkus/benchmark/filter/ServerHeaderFilter.java
+++ frameworks/Java/quarkus/base/src/main/java/io/quarkus/benchmark/filter/ServerHeaderFilter.java
@@ -1,45 +1,32 @@
 package io.quarkus.benchmark.filter;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
+import javax.inject.Singleton;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 
-import org.jboss.resteasy.util.HttpHeaderNames;
+import io.quarkus.scheduler.Scheduled;
 
+@Singleton
 @Provider
 public class ServerHeaderFilter implements ContainerResponseFilter {
 
-    private final DateHelper dateHelper = new DateHelper();
+    private String date;
+
+    @Scheduled(every="1s")
+    void increment() {
+        date = DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now());
+    }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-        responseContext.getHeaders().add("Server", "Quarkus");
-        responseContext.getHeaders().add(HttpHeaderNames.DATE, dateHelper.getDate());
-    }
-
-    static class DateHelper extends TimerTask {
-        private Timer timer = new Timer();
-        private String date = generateDate();
-
-        public DateHelper() {
-            timer.schedule(this, 0, 1000);
-        }
-
-        @Override
-        public void run() {
-            date = generateDate();
-        }
-
-        private String generateDate() {
-            return java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME.format(java.time.ZonedDateTime.now());
-        }
-
-        public String getDate() {
-            return date;
-        }
+        final MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        headers.add( "Server", "Quarkus");
+        headers.add( "Date", date);
     }
 }
