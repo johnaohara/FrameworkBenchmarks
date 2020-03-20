4c9a229b5922526165bc7ddc3444686407c6595e
Sanne Grinovero <sanne@hibernate.org>
2020-03-20 15:45:54 +0000

    Quarkus pgreactive: no longer needing to use schedulers for queries

--- frameworks/Java/quarkus/pgclient/src/main/java/io/quarkus/benchmark/repository/pgclient/FortuneRepository.java
+++ frameworks/Java/quarkus/pgclient/src/main/java/io/quarkus/benchmark/repository/pgclient/FortuneRepository.java
@@ -2,15 +2,12 @@ package io.quarkus.benchmark.repository.pgclient;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import io.quarkus.benchmark.model.Fortune;
-import io.reactivex.Scheduler;
 import io.reactivex.Single;
-import io.reactivex.schedulers.Schedulers;
 import io.vertx.reactivex.pgclient.PgPool;
 import io.vertx.reactivex.sqlclient.Row;
 import io.vertx.reactivex.sqlclient.RowSet;
@@ -20,8 +17,6 @@ public class FortuneRepository {
 
     @Inject
     PgPool client;
-
-    private final Scheduler scheduler = Schedulers.from(Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2));
 
     public Single<List<Fortune>> findAll() {
         return client.rxQuery("SELECT id, message FROM fortune")
@@ -34,7 +29,6 @@ public class FortuneRepository {
                         fortunes.add(fortune);
                     }
                     return fortunes;
-                })
-                .subscribeOn(scheduler);
+                });
     }
 }
--- frameworks/Java/quarkus/pgclient/src/main/java/io/quarkus/benchmark/repository/pgclient/WorldRepository.java
+++ frameworks/Java/quarkus/pgclient/src/main/java/io/quarkus/benchmark/repository/pgclient/WorldRepository.java
@@ -1,7 +1,5 @@
 package io.quarkus.benchmark.repository.pgclient;
 
-import java.util.concurrent.Executors;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -10,9 +8,7 @@ import org.slf4j.LoggerFactory;
 
 import io.quarkus.benchmark.model.World;
 import io.reactivex.Maybe;
-import io.reactivex.Scheduler;
 import io.reactivex.Single;
-import io.reactivex.schedulers.Schedulers;
 import io.vertx.reactivex.pgclient.PgPool;
 import io.vertx.reactivex.sqlclient.RowIterator;
 import io.vertx.reactivex.sqlclient.RowSet;
@@ -26,21 +22,17 @@ public class WorldRepository {
     @Inject
     PgPool client;
 
-    private final Scheduler scheduler = Schedulers.from(Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2));
-
     public Maybe<World> find(int id) {
         return client.rxPreparedQuery("SELECT id, randomnumber FROM world WHERE id = $1", Tuple.of(id))
                 .map(RowSet::iterator)
                 .filter(RowIterator::hasNext)
                 .map(RowIterator::next)
-                .map(row -> new World(row.getInteger(0), row.getInteger(1)))
-                .subscribeOn(scheduler);
+                .map(row -> new World(row.getInteger(0), row.getInteger(1)));
     }
 
     public Single<World> update(World world) {
         return client.rxPreparedQuery("UPDATE world SET randomnumber = $1 WHERE id = $2",
                 Tuple.of(world.getRandomNumber(), world.getId()))
-                .map(rows -> world)
-                .subscribeOn(scheduler);
+                .map(rows -> world);
     }
 }
