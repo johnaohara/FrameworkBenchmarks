eaa335b78f55258d450341c8efbc0e80952ad201
John OHara <johara@redhat.com>
2020-05-29 02:10:54 +0100

    Add required headers to hibernate reponses

---
+++ frameworks/Java/quarkus/hibernate/src/main/java/io/quarkus/benchmark/filter/ServerHeaderFilter.java
@@ -0,0 +1,39 @@
+package io.quarkus.benchmark.filter;
+
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.scheduler.Scheduled;
+import org.jboss.resteasy.util.HttpHeaderNames;
+
+import javax.enterprise.event.Observes;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Provider
+public class ServerHeaderFilter implements ContainerResponseFilter {
+
+    protected static String date;
+
+    private static final String SERVER = "Server";
+    private static final String QUARKUS = "Quarkus";
+
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        responseContext.getHeaders().add(SERVER, QUARKUS);
+        responseContext.getHeaders().add(HttpHeaderNames.DATE, date);
+    }
+
+
+    void onStart(@Observes StartupEvent ev) {
+        date = DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now());
+    }
+
+    @Scheduled(every="1s")
+    public void increment() {
+        date = DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now());
+    }
+}
