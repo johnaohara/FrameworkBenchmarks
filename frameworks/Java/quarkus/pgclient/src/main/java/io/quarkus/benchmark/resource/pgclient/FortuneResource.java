8a2d43d4ebdb3b0e7df922564cb024efc1f2d8d9
John OHara <johara@redhat.com>
2020-06-01 10:49:07 +0100

    Use Qute as template engine

--- frameworks/Java/quarkus/pgclient/src/main/java/io/quarkus/benchmark/resource/pgclient/FortuneResource.java
+++ frameworks/Java/quarkus/pgclient/src/main/java/io/quarkus/benchmark/resource/pgclient/FortuneResource.java
@@ -1,18 +1,14 @@
 package io.quarkus.benchmark.resource.pgclient;
 
-import java.io.StringWriter;
-import java.util.Collections;
 import java.util.Comparator;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheFactory;
-
 import io.quarkus.benchmark.model.Fortune;
 import io.quarkus.benchmark.repository.pgclient.FortuneRepository;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.api.ResourcePath;
 import io.quarkus.vertx.web.Route;
 import io.vertx.ext.web.RoutingContext;
 
@@ -22,12 +18,8 @@ public class FortuneResource extends BaseResource {
     @Inject
     FortuneRepository repository;
 
-    private final Mustache template;
-
-    public FortuneResource() {
-        MustacheFactory mf = new DefaultMustacheFactory();
-        template = mf.compile("fortunes.mustache");
-    }
+    @ResourcePath("fortunes")
+    Template template;
 
     @Route(path = "fortunes")
     public void fortunes(RoutingContext rc) {
@@ -35,10 +27,8 @@ public class FortuneResource extends BaseResource {
         .subscribe().with( fortunes -> {
             fortunes.add(new Fortune(0, "Additional fortune added at request time."));
             fortunes.sort(Comparator.comparing(fortune -> fortune.getMessage()));
-            StringWriter writer = new StringWriter();
-            template.execute(writer, Collections.singletonMap("fortunes", fortunes));
             rc.response().putHeader("Content-Type", "text/html;charset=UTF-8");
-            rc.response().end(writer.toString());
+            rc.response().end(template.data("fortunes.html", fortunes).render());
         },
                            t -> handleFail(rc, t));
     }
