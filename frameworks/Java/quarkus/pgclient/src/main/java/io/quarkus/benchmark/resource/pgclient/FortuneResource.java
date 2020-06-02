63e392255a37877a6e1f675c283af4d5d932cad0
john o'hara <johara@localhost.localdomain>
2020-06-02 11:58:51 +0100

    Fix fortunes qute example

--- frameworks/Java/quarkus/pgclient/src/main/java/io/quarkus/benchmark/resource/pgclient/FortuneResource.java
+++ frameworks/Java/quarkus/pgclient/src/main/java/io/quarkus/benchmark/resource/pgclient/FortuneResource.java
@@ -15,6 +15,10 @@ import io.vertx.ext.web.RoutingContext;
 @ApplicationScoped
 public class FortuneResource extends BaseResource {
 
+    private static final String CONTENT_TYPE = "Content-type";
+    private static final String TEXT_HTML = "text/html;charset=UTF-8";
+    private static final String FORTUNES_KEY = "fortunes";
+
     @Inject
     FortuneRepository repository;
 
@@ -24,12 +28,17 @@ public class FortuneResource extends BaseResource {
     @Route(path = "fortunes")
     public void fortunes(RoutingContext rc) {
         repository.findAll()
-        .subscribe().with( fortunes -> {
-            fortunes.add(new Fortune(0, "Additional fortune added at request time."));
-            fortunes.sort(Comparator.comparing(fortune -> fortune.getMessage()));
-            rc.response().putHeader("Content-Type", "text/html;charset=UTF-8");
-            rc.response().end(template.data("fortunes.html", fortunes).render());
-        },
-                           t -> handleFail(rc, t));
+                .subscribe().with(fortunes -> {
+                    fortunes.add(new Fortune(0, "Additional fortune added at request time."));
+                    fortunes.sort(Comparator.comparing(fortune -> fortune.getMessage()));
+                    rc.response().putHeader(CONTENT_TYPE, TEXT_HTML);
+                    template.data(FORTUNES_KEY, fortunes).renderAsync().exceptionally(err -> {
+                        handleFail(rc, err);
+                        return null;
+                    }).thenAccept(result -> {
+                        rc.response().end(result);
+                    });
+                },
+                t -> handleFail(rc, t));
     }
 }
